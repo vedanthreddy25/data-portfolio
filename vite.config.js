c27d3f9115de0e14f23feb75d397db736cc2112c
Vedanth Reddy Doddannagari <98727754+vedanthreddy25@users.noreply.github.com>
2025-11-21 11:35:07 -0600

    Modify vite.config.js for GitHub Pages
    Updated base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/data-portfolio/' : '/',
   plugins: [react()],
+})*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/vedanthreddy25/'   // REQUIRED for GitHub Pages
 })
